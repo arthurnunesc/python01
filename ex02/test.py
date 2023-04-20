cff7b230eb9eaa6368f231c62ef83cb6e738fc2e
Arthur Nunes <arthurnunesc@proton.me>
2023-04-20 18:55:14 +0200

    Add ex02 exercise done

--- ex02/test.py
+++ ex02/test.py
@@ -2,7 +2,41 @@ from vector import Vector
 
 
 def main():
-    pass
+    vector_column_1 = Vector([[1.5], [2.5], [3.5]])
+    vector_column_2 = Vector([[2], [2], [2]])
+    print(vector_column_1.__dict__)
+    print(vector_column_2.__dict__)
+    print(vector_column_1.dot(vector_column_2))
+    print(vector_column_1.T())
+
+    vector_row_1 = Vector([[1.5, 2.5, 3.5]])
+    vector_row_2 = Vector([[2, 2, 2]])
+    print(vector_row_1.__dict__)
+    print(vector_row_2.__dict__)
+    print(vector_row_1.dot(vector_row_2))
+    print(vector_row_1.T())
+    
+    # Column vector of shape n * 1
+    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    v2 = v1 * 5
+    print(v2)
+    # Vector([[0.0], [5.0], [10.0], [15.0]])
+
+    # Row vector of shape 1 * n 
+    v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+    v2 = v1 * 5
+    # Vector([[0.0, 5.0, 10.0, 15.0]])
+
+    v2 = v1 / 2.0
+    print(v2)
+    # Vector([[0.0], [0.5], [1.0], [1.5]])
+
+    v1 / 0.0
+    # ZeroDivisionError: division by zero.
+
+    2.0 / v1
+    # NotImplementedError: Division of a scalar by a Vector is not defined here.
+
 
 if __name__ == "__main__":
     main()
---
+++ ex02/vector.py
@@ -0,0 +1,122 @@
+from typing import List
+
+
+def shape(vector: List[List[float]]):
+    rows = 0
+    cols = 0
+    cols_aux = 0
+    for row in vector:
+        if cols == 0:
+            for col in row:
+                cols += 1
+        else:
+            for col in row:
+                cols_aux += 1
+        if cols_aux != 0 and cols != cols_aux:
+            return False
+        cols_aux = 0
+        rows += 1
+    return (rows, cols)
+
+
+def validate_vector(vector: List[List[float]]):
+    validated_vector = vector
+    for list in validated_vector:
+        for number in list:
+            if type(number) is float:
+                continue
+            elif type(number) is int:
+                number = float(number)
+            else:
+                return False
+    return validated_vector
+
+
+class Vector:
+    def __init__(self, values: List[List[float or int]]):
+        self.values = validate_vector(values)
+        if self.values:
+            pass
+        else:
+            raise AssertionError("Vector has values that are not numbers")
+
+        self.shape = shape(values)
+        if self.shape:
+            pass
+        else:
+            raise AssertionError("Assymetrical vector")
+
+    def dot(self, other):
+        if not isinstance(other, Vector):
+            raise AssertionError("Argument is not a Vector object")
+        elif self.shape != other.shape:
+            raise AssertionError("The shapes of the vectors are different")
+        cols, rows = self.shape
+        result_vector: List[List[float]] = [[]]
+        if cols > 1:
+            result_vector = [[None]] * cols
+            for index in range(cols):
+                result_vector[[index][0]] = [
+                    self.values[index][0] * other.values[index][0]
+                ]
+        else:
+            result_vector = [[None] * rows]
+            for index in range(rows):
+                result_vector[0][index] = self.values[0][index] * other.values[0][index]
+
+        return result_vector
+
+    def T(self, return_shape=False):
+        cols, rows = self.shape
+        transposed_vector: List[List[float]] = [[]]
+        if cols > 1:
+            transposed_vector = [[None] * cols]
+            for index in range(cols):
+                transposed_vector[0][index] = self.values[index][0]
+        else:
+            transposed_vector = [[None]] * rows
+            for index in range(rows):
+                transposed_vector[[index][0]] = [self.values[0][index]]
+        if not return_shape:
+            return transposed_vector
+        else:
+            return transposed_vector, shape(transposed_vector)
+
+    def __mul__(self, number: int or float):
+        cols, rows = self.shape
+        result_vector: List[List[float]] = [[]]
+        if cols > 1:
+            result_vector = [[None]] * cols
+            for index in range(cols):
+                result_vector[[index][0]] = [self.values[index][0] * number]
+        else:
+            result_vector = [[None] * rows]
+            for index in range(rows):
+                result_vector[0][index] = self.values[0][index] * number
+
+        return result_vector
+
+    def __rmul__(self, number):
+        raise NotImplementedError(
+            "Multiplication of a scalar by a Vector is not defined here."
+        )
+
+    def __truediv__(self, number: int or float):
+        cols, rows = self.shape
+        result_vector: List[List[float]] = [[]]
+        if cols > 1:
+            result_vector = [[None]] * cols
+            for index in range(cols):
+                result_vector[[index][0]] = [self.values[index][0] / number]
+        else:
+            result_vector = [[None] * rows]
+            for index in range(rows):
+                result_vector[0][index] = self.values[0][index] / number
+
+        return result_vector
+
+    def __rtruediv__(self, number):
+        raise NotImplementedError(
+            "Division of a scalar by a Vector is not defined here."
+        )
+
